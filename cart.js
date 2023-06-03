5d93a3c01d2a92de0964cf11edf8ef1bbcc3de57
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-06-02 21:27:45 -0600

    cart.js almost complete --> moving to orders.js for now

--- cart.js
+++ cart.js
@@ -33,10 +33,7 @@ const cart = [
     }
 ]
 
-//CODE HERE
-
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
-
+const summedPrice = cart.reduce((acc, curr) => acc + curr.price, 0)
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -53,9 +50,7 @@ const cart = [
     decimals, for example: .06 for a 6% tax.
 */
 
-//CODE HERE
-
-
+const calcFinalPrice = (cartTotal, couponValue, tax) => (cartTotal * (tax + 1) - couponValue).toFixed(2)
 
 //////////////////PROBLEM 3////////////////////
 /*  
